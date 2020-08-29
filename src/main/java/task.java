1c661d9071cd96f85b32acada5d86cce7416c63b
e0406981 <e0406981@u.nus.edu>
2020-08-29 22:24:28 +0800

    added task class

--- src/main/java/task.java
+++ src/main/java/task.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class task {
+public class task {
+    private String name;
+    private Integer number;
+    private boolean isDone;
+
+    public task(String name, Integer number, boolean isDone){
+        setName(name);
+        setNumber(number);
+        setDone(isDone);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Integer getNumber(){
+        return number;
+    }
+
+    public boolean getIsDone(){
+        return isDone;
+    }
 }
